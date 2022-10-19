66d8a2194166c7d7cebe8523956a01ec3d95977b
Ken1567 <garrovillomanuelkennon@gmail.com>
2022-10-20 01:58:55 +0800

    Add paragraphs

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -19,8 +19,24 @@ const Home = () => {
         safety protocols and driven by the shared cleaning expertise of all ServiceMaster Brands, 
         you can rest assured that your home receives a cleaning backed by decades of experience.
         </p>
+        <h3>House cleaning services you can see and feel</h3>
+        <p>
+        Your home is more than just a house—it’s where memories are made and moments are cherished. 
+        Though life can be complicated, the simple things shouldn’t be. Cleaners strives to take the 
+        stress out of your day so you can do life your way. With more than 40 years of experience and an 
+        advanced, time-tested cleaning process, we can help you reclaim time with your loved ones.
+        </p>
+        <h3>Our Worry-Free Guarantee</h3>
+        <p>
+        When you trust your space to the team at Cleaners®, you can rest easy knowing your home is in great 
+        hands. You deserve to feel confident knowing you’ll receive consistently flawless results from friendly, 
+        trustworthy professionals. Cleaners® provides the relief you are looking for. Our Worry-Free Guarantee 
+        reflects our willingness to go the extra mile to ensure your home always feels like your sanctuary. 
+        If for any reason you are unhappy with our recent visit, let us know within 24-hours and we’ll come 
+        back and make it right.
+        </p>
     </div>
   )
 }
 
-export default Home
+export default Home;
