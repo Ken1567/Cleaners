3c6ee16a92fcf5eec21b7bd1b029d40737717b92
Ken1567 <garrovillomanuelkennon@gmail.com>
2022-10-20 15:04:20 +0800

    Adjust header and paragraph margin

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,8 +6,8 @@ const Home = () => {
     <div>
         <Hero />
         <br />
-        <h3>Keep a clean and healthy environment with our enhanced disinfection services</h3>
-        <p>
+        <h3 className="m-1">Keep a clean and healthy environment with our enhanced disinfection services</h3>
+        <p className="m-2">
         Cleaners is committed to creating a clean and healthy environment for you and your family. 
         In addition to our exceptional routine cleaning, enhanced disinfection services are available for 
         your home to help ensure the wellness of your family.
@@ -19,15 +19,15 @@ const Home = () => {
         safety protocols and driven by the shared cleaning expertise of all ServiceMaster Brands, 
         you can rest assured that your home receives a cleaning backed by decades of experience.
         </p>
-        <h3>House cleaning services you can see and feel</h3>
-        <p>
+        <h3 className="m-1">House cleaning services you can see and feel</h3>
+        <p className="m-2">
         Your home is more than just a house—it’s where memories are made and moments are cherished. 
         Though life can be complicated, the simple things shouldn’t be. Cleaners strives to take the 
         stress out of your day so you can do life your way. With more than 40 years of experience and an 
         advanced, time-tested cleaning process, we can help you reclaim time with your loved ones.
         </p>
-        <h3>Our Worry-Free Guarantee</h3>
-        <p>
+        <h3 className="m-1">Our Worry-Free Guarantee</h3>
+        <p className="m-2">
         When you trust your space to the team at Cleaners®, you can rest easy knowing your home is in great 
         hands. You deserve to feel confident knowing you’ll receive consistently flawless results from friendly, 
         trustworthy professionals. Cleaners® provides the relief you are looking for. Our Worry-Free Guarantee 
